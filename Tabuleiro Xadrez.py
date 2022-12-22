6dc9eab464ac115ffeb5df8648d80921b4d28786
Bruno Palhano <107354811+jbrunopg@users.noreply.github.com>
2022-12-22 18:04:40 -0300

    Add files via upload

---
+++ Tabuleiro Xadrez.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.colors import ListedColormap
+
+tabuleiro = np.tile([1, 0], (8, 4))
+
+for i in range(tabuleiro.shape[0]):
+    tabuleiro[i] = np.roll(tabuleiro[i], i % 2)
+
+mapa_cores = ListedColormap(['#000000', '#FFFFFF'])
+plt.matshow(tabuleiro, cmap=mapa_cores)
+plt.xticks([])
+plt.yticks([])
+plt.show()
+
